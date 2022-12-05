8c81c62ce4fec0d8ef306bb0be591f7a87a972cf
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2022-12-05 07:02:20 +0300

    touches, etc.

--- cntrl/story.js
+++ cntrl/story.js
@@ -4,5 +4,6 @@ let router = express.Router();
 const storyService = require('../srvc/story')
 
 router.post(`/story`, express.json(), storyService.newStory)
+router.get(`/story`, express.json(), storyService.getStories)
 
 module.exports = router;
--- srvc/story.js
+++ srvc/story.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const _FILENAME = path.basename(__filename);
 
+const axios = require('axios');
 require('dotenv').config();
-const { MY_ALCHEMY_API_KEY, MY_METAMASK_PRIVATE_KEY, ALCHEMY_API_BASE_URL, HW_CONTRACT_ADDRESS } = process.env;
+const { MY_ALCHEMY_API_KEY, MY_METAMASK_PRIVATE_KEY, ALCHEMY_API_BASE_URL, HW_CONTRACT_ADDRESS, POLYGON_MUMBAI } = process.env;
+const { Alchemy, Network } = require("alchemy-sdk");
 
-const { ethers } = require("ethers");
+const { ethers, providers } = require("ethers");
+
+const web3 = require('web3');
 
 // For Hardhat
 const _blowWhistleContract = require("../artifacts/contracts/BlowWhistle.sol/BlowWhistle.json");
@@ -30,11 +34,11 @@ exports.newStory = async (req, res) => {
         const _FUNCTIONNAME = 'newStory'
         console.log('hitting', _FILENAME, _FUNCTIONNAME);
         console.log('Body', req.body);
-    
+
         console.log("Updating the story...");
         const tx = await blowWhistleContract.newStory(JSON.stringify(req.body));
         await tx.wait();
-    
+
         res.status(200).json({
             message: 'GOOD'
         })
@@ -42,6 +46,68 @@ exports.newStory = async (req, res) => {
         res.status(503).json({
             message: 'BAD',
             error
+        })
+    }
+}
+
+exports.getStories = async (req, res) => {
+
+
+    try {
+        const _FUNCTIONNAME = 'getStories'
+        console.log('hitting', _FILENAME, _FUNCTIONNAME);
+        const config = {
+            apiKey: MY_ALCHEMY_API_KEY,
+            network: Network.MATIC_MUMBAI,
+        };
+        const alchemy = new Alchemy(config);
+
+        const data = await alchemy.core.getLogs({
+            fromBlock: "0x0",
+            address: HW_CONTRACT_ADDRESS,
+        });
+
+        // ethers.utils.
+
+        // alchemyProvider.getLogs({})
+        
+        function hex2a(hexx) {
+            let hex = hexx.toString();//force conversion
+            let str = '';
+            for (let i = 0; i < hex.length; i += 2)
+                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+            return str;
+        }
+
+        // const data = await axios.post(`${POLYGON_MUMBAI}`, {
+        //     "jsonrpc": "2.0",
+        //     "id": 0,
+        //     "method": "alchemy_getAssetTransfers",
+        //     "params": [
+        //         {
+        //             "fromBlock": "0x0",
+        //             "fromAddress": HW_CONTRACT_ADDRESS,
+        //         }
+        //     ]
+        // })
+
+        // for (let index = 0; index < data.length; index++) {
+        //     let element = data[index];
+        //     element.data = web3.utils.hexToAscii(element.data) // hex2a(element.data)
+            
+        // }
+
+        res.status(200).json({
+            message: 'GOOD',
+            data
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(503).json({
+            message: 'BAD',
+            error: error
         })
     }
 }
